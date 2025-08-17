c830e1338fc705590d98599cf3aee830c0871a64
dmardoqueu <diogomardoqueu@gmail.com>
2025-08-16 23:50:20 -0300

    Instalando o Prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify';
+import { PrismaClient } from 'generated/prisma/index.js';
 
 export const app = fastify()
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+    data: {
+        name: 'Diogo Mardoqueu',
+        email: 'diogovasco11@gmail.com',
+    }
+})
