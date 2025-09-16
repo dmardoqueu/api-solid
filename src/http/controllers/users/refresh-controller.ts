5595bf30bf6808e404d7742877f5bfd972254b7e
dmardoqueu <diogomardoqueu@gmail.com>
2025-09-15 23:42:22 -0300

    Implementando refresh token

---
+++ src/http/controllers/users/refresh-controller.ts
@@ -0,0 +1,41 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function refresh(
+    request: FastifyRequest,
+    reply: FastifyReply,
+) {
+
+    await request.jwtVerify({ onlyCookie: true }) // só verifica o cookie
+
+    const token = await reply.jwtSign(
+        {},
+        {
+            sign: {
+                sub: request.user.sub,
+            },
+        },
+    )
+
+    const refreshToken = await reply.jwtSign(
+        {},
+        {
+            sign: {
+                sub: request.user.sub,
+                expiresIn: '7d',
+            },
+        },
+    )
+
+    return reply
+        .setCookie('refreshToken', refreshToken, {
+            path: '/',
+            secure: true, // only use in https
+            sameSite: true, // csrf
+            httpOnly: true, // no access via js
+            maxAge: 60 * 60 * 24 * 7, // 7 days
+        })
+        .status(200)
+        .send({
+            token,
+        })
+}
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,14 @@ import { authenticate } from '@/http/controllers/users/authenticate-controller.j
 import { register } from './register-controller.js'
 import { profile } from '@/http/controllers/users/profile-controller.js'
 import { verifyJwt } from '../../middlewares/verify-jwt.js'
+import { refresh } from './refresh-controller.js'
 
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJwt] }, profile)
