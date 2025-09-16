2b0d6e2b1c65c025531f3a92b2b293bde58934e6
dmardoqueu <diogomardoqueu@gmail.com>
2025-09-16 00:38:35 -0300

    Implementando autorização por cargos

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -5,6 +5,7 @@ import { validate } from './validate-controller.js'
 import { metrics } from './metrics-controller.js'
 import { history } from './history-controller.js'
 import { create } from './create-controller.js'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role.js'
 
 export async function checkInsRoutes(app: FastifyInstance) {
     app.addHook('onRequest', verifyJwt)
@@ -13,5 +14,5 @@ export async function checkInsRoutes(app: FastifyInstance) {
     app.get('/check-ins/metrics', metrics)
 
     app.post('/gyms/:gymId/check-ins', create)
-    app.patch('/check-ins/:checkInId/validate', validate)
+    app.patch('/check-ins/:checkInId/validate', { onRequest: [verifyUserRole('ADMIN')] }, validate)
 }
--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,6 +3,7 @@ import { verifyJwt } from '../../middlewares/verify-jwt.js'
 import { search } from './search-controller.js'
 import { nearby } from './nearby-controller.js'
 import { create } from './create-controller.js'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role.js'
 
 
 export async function gymsRoutes(app: FastifyInstance) {
@@ -11,5 +12,5 @@ export async function gymsRoutes(app: FastifyInstance) {
     app.get('/gyms/search', search)
     app.get('/gyms/nearby', nearby)
     
-    app.post('/gyms', create)
+    app.post('/gyms',{ onRequest: [verifyUserRole('ADMIN')] }, create)
 }
